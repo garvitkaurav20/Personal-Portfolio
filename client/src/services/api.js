30fc9a9bb37392de22b0e917b1fe371ceffd5c78
Garvit Kaurav <garvitkaurav@gmail.com>
2026-08-21 15:26:17 +0530

    feat: add Vercel SPA routing and Render deployment configuration, fix CSS background-clip

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,6 @@
-const API_BASE = '/api';
+const API_BASE = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api`
+  : '/api';
 
 function getAuthHeader() {
   const token = localStorage.getItem('garvit_portfolio_token');
